c8d953ecceba29b231ca3c2cad8414793ec8c7b3
Manoj Soni <manojncsoni@gmail.com>
2022-04-03 10:21:18 +0530

    refactor: Homepage code changed on given suggestion in PR

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
-import { CartProvider } from './context/cart-context'
+import { CartProvider } from './context/cart-context';
+import { BrowserRouter } from "react-router-dom";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
     <CartProvider>
     <App />
     </CartProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
